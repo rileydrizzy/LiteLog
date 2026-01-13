510d59284f6610f05a7ffa4587184391d18e5d9b
Ladipo Ezekiel Ipadeola <105759894+rileydrizzy@users.noreply.github.com>
2026-01-13 12:22:47 +0000

    docs: update def.h documentation and add documentation for Product struct

--- src/def.h
+++ src/def.h
@@ -1,6 +1,17 @@
+/**
+ * @file def.h
+ * @author Ladipo Ipadeola
+ * @brief This file contains the definitions for the inventory management system.
+ * @version 0.1
+ * @date 01-01-2026
+ * @copyright Copyright (c) 2026
+ */
+
+
 #ifndef DEF_H
 #define DEF_H
 
+// Color codes for the terminal
 #define BLUE "\033[34m"
 #define BOLD "\033[1m"
 #define RESET "\033[0m"
@@ -11,7 +22,7 @@
 #define WRITE_MODE "ab+"  
 #define UPDATE_MODE "rb+"
 
-
+// Width of the columns
 #define W_ID 6
 #define W_NAME 20
 #define W_QTY 10
@@ -19,7 +30,13 @@
 
 /**
  * @brief Represents a single Product in the database.
- */
+ * This structure holds the details of a product, including its ID, name, quantity, and price.
+ * It is used to store and retrieve product information from the database.
+ * @note The name field is limited to 30 characters.
+ * @note The price field is a floating-point number.
+ * @note The quantity field is an integer.
+ * @note The product_id field is an integer.
+*/
 typedef struct
 {
 	int product_id; ///< Unique Product ID number
@@ -30,7 +47,10 @@ typedef struct
 
 /**
 * @brief
-*
+* This structure holds the total number of products and the total value of the products.
+* It is used to store and retrieve the total number of products and the total value of the products.
+* @note The total_no_prod field is an integer.
+* @note The total_val field is a floating-point number.
 */
 typedef struct
 {
